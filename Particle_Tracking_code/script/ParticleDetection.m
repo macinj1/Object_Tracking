7775e114a39297f7c39278d358620950796aeb07
Jona Mac Intyre <64064916+macinj1@users.noreply.github.com>
2023-12-20 17:29:14 +0200

    Update ParticleDetection.m

--- Particle_Tracking_code/script/ParticleDetection.m
+++ Particle_Tracking_code/script/ParticleDetection.m
@@ -1,12 +1,63 @@
-function [CropArea,data,FrameNumber] = ParticleDetection(file,FirstFrame,LastFrame,FrameRate,showVideo) 
+function [CropArea,data,FrameNumber,spaceScale] = ParticleDetection(file,FirstFrame,LastFrame,FrameRate,showVideo) 
+
+%% Load Video 
 
 vi = VideoReader( file ) ;
 
-%%
+%% Select area of interest 
 
 [~,CropArea] = imcrop( read(vi,1) ) ; 
 
-%%
+%% Enter dimension space
+
+errorType = 1 ; 
+
+while errorType == 1 
+
+    prompt = "From the area you cropped, enter one dimension in meters or equivalent unit of the rectangle: width or height of rectangle: ";
+
+    pxlsScale = input(prompt) ; 
+
+    prompt = "Which dimension was it: width = type 1 || height = type 0: ";
+
+    scaleDirection = input(prompt) ; 
+
+    prompt = "Do you want to modify the scale you just typed: yes = type 1 || no = type 0 : ";
+
+    errorType = input(prompt) ; 
+
+    clc
+
+end
+
+clear errorType
+
+if scaleDirection == 1
+
+    spaceScale = pxlsScale / CropArea(3) ; 
+
+else
+
+    spaceScale = pxlsScale / CropArea(4) ; 
+
+end
+
+clear scaleDirection pxlsScale
+
+
+%% Mean value for substraction
+
+clear MFrame
+
+for k = 1:floor( vi.NumFrames/25 )
+
+    MFrame(:,:,k) = rgb2gray( imcrop( read(vi,k) , CropArea )  ) ;
+
+end
+
+MFrame = mean( MFrame , 3 ) ;
+
+%% Detect particles in each frame
 
 FrameNumber = [ ] ; 
 
@@ -16,7 +67,9 @@ for k = FirstFrame:FrameRate:LastFrame
 
     fig = imcrop( read(vi,k) , CropArea ) ;
 
-    centers = imageProcessing(fig) ; 
+    % centers = imageProcessing(fig) ; 
+
+    centers = imageProcessingMean(fig,MFrame) ; 
 
     data{k,1} = centers ; 
 
